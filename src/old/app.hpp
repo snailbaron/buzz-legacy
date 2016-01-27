40061e2560d514b497ef4e2369cdb8ca724da031
Igor Martynov <murbidodrus@gmail.com>
2016-01-27 03:08:36 +0300

    Add pre-OpenGL buzz stuff

---
+++ src/old/app.hpp
@@ -0,0 +1,29 @@
+#ifndef _APP_HPP_
+#define _APP_HPP_
+
+#include <Windows.h>
+#include "game.hpp"
+
+class App
+{
+public:
+    bool InitInstance(HINSTANCE hInstance, LPSTR cmdLine);
+
+    Game * CreateGameAndView();
+
+private:
+    HMODULE m_langDll;
+    HINSTANCE m_hInstance;
+    bool m_isRunning;
+
+
+    // TODO: m_gameOptions;
+    
+
+    
+
+
+
+};
+
+#endif
---
+++ src/old/game.hpp
@@ -0,0 +1,8 @@
+#ifndef _GAME_HPP_
+#define _GAME_HPP_
+
+class Game
+{
+};
+
+#endif
